cfd8439623a00ec4d0ed1332ff3a3d20e5640be1
Eoin Gorman <eoin@browserstack.com>
2019-12-13 18:04:00 +0000

    add config file for ids

--- server.rb
+++ server.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'sinatra/cross_origin'
 require 'json'
+require 'yaml'
 require_relative 'src/bamboo_api'
 require_relative 'src/employee'
 
 class DCGlobalServer < Sinatra::Base
   API = ::BambooApi.new('51b173b2561d92e8efa344d6345f56f656300683')
-  EMPLOYEE_IDS = [41276]
+  EMPLOYEE_IDS = YAML.load_file("#{File.dirname(__FILE__)}/config/employees.yml")
   set :bind, '0.0.0.0'
 
   configure do
