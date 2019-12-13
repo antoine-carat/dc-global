7b880fab8caa26ccedf499bd078e554e820b223e
Eoin Gorman <eoin@browserstack.com>
2019-12-13 14:13:33 +0000

    Adding BambooHR API key to server.rb

--- server.rb
+++ server.rb
@@ -3,6 +3,8 @@ require 'sinatra/cross_origin'
 require 'json'
 
 class DCGlobalServer < Sinatra::Base
+  API_KEY = '51b173b2561d92e8efa344d6345f56f656300683'
+
   set :bind, '0.0.0.0'
 
   configure do
@@ -17,5 +19,4 @@ class DCGlobalServer < Sinatra::Base
     status 200
     File.read(File.join('public', 'index.html'))
   end
-
 end
